09456ad348120cc4de2a4132f3bc909fb865bd2a
Eddy Frank Fotsing Kamboh <efotsing88@gmail.com>
2022-06-07 21:42:06 -0400

    Kaggle Notebook | MATH1281 | Version 1

---
+++ math1281.r
@@ -0,0 +1,117 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3bc1eb3e",
+   "metadata": {
+    "_cell_guid": "babea9bd-afa6-43a7-9c00-b2c6372f0bd2",
+    "_uuid": "18eff10b-3b85-4718-a40a-3a040ca543d4",
+    "collapsed": false,
+    "execution": {
+     "iopub.execute_input": "2022-02-10T02:01:08.887454Z",
+     "iopub.status.busy": "2022-02-10T02:01:08.885367Z",
+     "iopub.status.idle": "2022-02-10T02:01:09.022232Z"
+    },
+    "jupyter": {
+     "outputs_hidden": false
+    },
+    "papermill": {
+     "duration": 0.008111,
+     "end_time": "2022-06-08T01:41:55.000465",
+     "exception": false,
+     "start_time": "2022-06-08T01:41:54.992354",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3b4e8af4",
+   "metadata": {
+    "_cell_guid": "e4f2ff9b-80b0-4c45-975f-ecdc627e28f3",
+    "_uuid": "40ca1efd-c695-4cd3-bbe8-1970fbf4fa7e",
+    "collapsed": false,
+    "execution": {
+     "iopub.execute_input": "2022-06-07T22:14:30.488393Z",
+     "iopub.status.busy": "2022-06-07T22:14:30.486277Z",
+     "iopub.status.idle": "2022-06-07T22:14:33.067204Z"
+    },
+    "jupyter": {
+     "outputs_hidden": false
+    },
+    "papermill": {
+     "duration": 0.007029,
+     "end_time": "2022-06-08T01:41:55.014956",
+     "exception": false,
+     "start_time": "2022-06-08T01:41:55.007927",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0d320d7b",
+   "metadata": {
+    "_cell_guid": "391f6a1e-8b84-4bd6-8128-96a704b9924d",
+    "_uuid": "4dd8103b-bbd6-4c24-8f75-43992142989c",
+    "collapsed": false,
+    "execution": {
+     "iopub.execute_input": "2022-06-07T22:19:45.685361Z",
+     "iopub.status.busy": "2022-06-07T22:19:45.683605Z",
+     "iopub.status.idle": "2022-06-07T22:19:47.335444Z"
+    },
+    "jupyter": {
+     "outputs_hidden": false
+    },
+    "papermill": {
+     "duration": 0.006994,
+     "end_time": "2022-06-08T01:41:55.029035",
+     "exception": false,
+     "start_time": "2022-06-08T01:41:55.022041",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.196245,
+   "end_time": "2022-06-08T01:41:55.147513",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2022-06-08T01:41:51.951268",
+   "version": "2.3.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
